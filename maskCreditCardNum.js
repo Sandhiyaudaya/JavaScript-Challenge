32c0eefe0cdcc358aa1f5529c6ade2bc5ab364ee
Sandhiya <sandhiya.u@gmail.com>
2022-05-10 15:23:40 +0530

    Masking the Credit Card Number

---
+++ maskCreditCardNum.js
@@ -0,0 +1,22 @@
+/*Usually when you buy something, you're asked whether your credit card 
+number, phone number or answer to your most secret question is 
+still correct. However, since someone could look over your shoulder, 
+you don't want that shown on your screen. Instead, we mask it.
+Your task is to write a function maskify, 
+which changes all but the last four characters into '#'.*/
+
+function maskify(cc) {
+    visa = cc.slice(-4);
+    countNum = '';
+
+    for (i = cc.length - 4; i > 0; i--) {
+        countNum += '#';
+    }
+    return (countNum + visa)
+}
+
+//Another method
+
+function maskify(cc) {
+    return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
+  }
