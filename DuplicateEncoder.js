063e8ea1978a047d7271d0c4a37ad76c948cc7ff
Sandhiya <sandhiya.u@gmail.com>
2022-05-17 17:01:33 +0530

    Repeated word encoder

---
+++ DuplicateEncoder.js
@@ -0,0 +1,28 @@
+/*The goal of this exercise is to convert a string to a new string where each character in the new string is
+ "(" if that character appears only once in the original string, or ")" if that character appears more 
+ than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+Notes
+Assertion messages may be unclear about what they display in some languages. 
+If you read "...It Should encode XXX", the "XXX" is the expected result, not the input! */
+
+function duplicateEncode(word) {
+    let str = word.toLowerCase().split("");
+    let uniq = '';
+
+    for (i = 0; i < str.length; i++) {
+        if (str.indexOf(str[i]) !== str.lastIndexOf(str[i])) {
+            uniq += ')';
+        } else {
+            uniq += '(';
+        }
+    }
+
+    return uniq;
+
+}
